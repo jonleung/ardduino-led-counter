0d94265745ae2c314327e11c38ff2dd603f37b09
Jonathan Leung <me@jonl.org>
2016-07-01 11:49:18 -0400

    mvp 1 demo for incrementing LEDs via touch

--- led_counter.ino
+++ led_counter.ino
@@ -3,7 +3,13 @@ int LED_PIN_OUT[] = {3, 5, 6, 9, 10, 11}; // 3, 5, 6, 9, 10, 11 // 2,3,4,5,6,7
 //                                   └ right most
 int LED_PIN_OUT_LEN = (int) (sizeof(LED_PIN_OUT)/sizeof(LED_PIN_OUT[0]));
 
+#define ANALOG_IN A0
+#define TOUCH_SENSOR_THRESHOLD 800
+#define TOUCH_COUNTER_THRESHOLD 10000
+
 int numOn;
+int touchCounter;
+int turnedOnOne;
 
 void setup() {
   Serial.begin(9600);
@@ -15,11 +21,40 @@ void setup() {
   }
   
   numOn = 0;
+  turnedOnOne = false;
 }
 
+void loop() {
+  // Button Counter
+  int touchSensorValue = analogRead(ANALOG_IN);
+  if (touchSensorValue > TOUCH_SENSOR_THRESHOLD && turnedOnOne == false) {
+    touchCounter++;
+    int nextPin = LED_PIN_OUT[numOn];
+    int brightness = 255 * (touchCounter + 2000) / TOUCH_COUNTER_THRESHOLD;
+//    Serial.println(brightness)0
+    analogWrite(nextPin, brightness);
+  }
+  else {
+    if (touchCounter >= 0) {
+      touchCounter -= 1000;
+      if (touchCounter < 0) {
+        touchCounter = 0;
+        turnedOnOne = false;
+      }
+    }
+  }
 
-
-void loop() {  
+  if (touchCounter > TOUCH_COUNTER_THRESHOLD && turnedOnOne == false) {
+    numOn++;
+    if (numOn == LED_PIN_OUT_LEN+1) {
+      numOn = 0;
+    }
+    
+    touchCounter = 0;
+    turnedOnOne = true;
+  }
+  
+  // Turnon appropriate LEDs based on numOn
   int curPin = LED_PIN_OUT[0];
   for(int i = 0; i<LED_PIN_OUT_LEN; i++) {
       int curPin = LED_PIN_OUT[i];
@@ -30,11 +65,5 @@ void loop() {
       analogWrite(curPin, 0);
     }
   }
-  
-  numOn++;
-  if(numOn == LED_PIN_OUT_LEN+1) {
-    numOn = 0;
-  }
-  
-  delay(100);
+    
 }
